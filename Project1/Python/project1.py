13dd44e518da77f043f10d1eb13293e7ffbf22c8
RomainCorbel <romain.corbel@hotmail.com>
2026-04-29 15:13:54 +0200

    project1.py ready to run

--- Project1/Python/project1.py
+++ Project1/Python/project1.py
@@ -5,13 +5,13 @@ from farms_core import pylog
 from exercise_all import exercise_all
 
 
-def main(parallel=True): 
+def main(parallel=False): 
     """Main function that runs all the exercises."""
 
     pylog.info('Running simulation exercises')
 
-    # arguments = ["1_1", "1_2", "2_1", "2_2", "2_3", "3_1", "3_2", "3_3"]
-    arguments = ["1_1", "1_2", "2_1", "2_2", "2_3", "3_1", "3_2"]
+    arguments = ["1_1", "1_2", "2_1", "2_2", "2_3", "3_1", "3_2", "3_3"]
+    
     if parallel:
         with Pool() as pool:  # Pool(processes=4)
             pool.map(exercise_all, [[arg] for arg in arguments])
